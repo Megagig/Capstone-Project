af773ce1079974ba75bc470cea3977dab9afc4bc
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-05-14 15:51:45 +0100

    Remove commented codes

--- script.js
+++ script.js
@@ -1,95 +1,94 @@
 const hamburger = document.querySelector('.hambuger');
 const navMenu = document.querySelector('.nav-links');
 
-function mobileMenu() {
-  if (navMenu.classList.contains('way')) {
-    navMenu.classList.remove('way');
-    hamburger.classList.toggle('active');
-  } else {
-    navMenu.classList.add('way');
-    hamburger.classList.toggle('active');
-  }
-}
+const mobileMenu = () => {
+  navMenu.classList.toggle('way');
+  hamburger.classList.toggle('active');
+};
+
 hamburger.addEventListener('click', mobileMenu);
-document.querySelectorAll('.links').forEach((n) =>
-  n.addEventListener('click', () => {
+
+document.querySelectorAll('.links').forEach((link) => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('way');
     hamburger.classList.remove('active');
-    navMenu.classList.add('way');
-    hamburger.toggle('line active');
-  })
-);
+  });
+});
 
 // CREATING FEATURED SPEAKER SECTION DYNAMICALLY
 // Select the speakers element using the class name
 
 // Define an array of speaker details
+
 const speakersDetails = [
   {
     id: 1,
-    name: 'Pharm. Folashade Lawal',
-    title: 'CEO, Victory Drugs Limited.',
+    name: 'Mr Akintunde Adelana',
+    title: 'Director, Monitoring and Evaluation, NCDMB',
     speakerImg: 'image/akintunde-adelana.jpg',
     about:
-      'Pharm. Folash Lawal is the CEO of Victory Drugs Limited. A leading community Pharmacy in FESTAC Town, Lagos',
+      'Mr Akintunde Adelana is the Director, Monitoring and Evaluation, NCDMB. He is featured in many peer-reviewed journals.',
   },
   {
     id: 2,
-    name: 'Dr. Peter Bamkole',
-    title: 'Physician, Hilton-Top Hospital Ikoyi, Lagos.',
+    name: 'Alero Onosode',
+    title: 'Chairperson - Diversity Sectoral Working Group',
     speakerImg: 'image/alero-onosode.jpg',
     about:
-      'Peter Bamkole, pioneered the Enterprise Development Centre (EDC) of the Pan-Atlantic University in January 2003, now one of the top enterprise development centers in Africa.',
+      'The chairman of the Nigerian Content Consultative Forum. Astute member of the Nigerian Society of Engineers.',
   },
   {
     id: 3,
-    name: 'Pharm. Bisi Bright',
-    title: 'Consultant Clinical Pharmacist and Public Health Manager.',
+    name: 'Dr Ama Ikuru',
+    title: 'Ag. Director, Capacity Building, NCDMB',
     speakerImg: 'image/dr-ama-ikuru.jpg',
     about:
-      'Pharm. (Dr) Mrs. Bisi Bright is a Consultant Clinical Pharmacist and Public Health Manager. A fellow of both the Pharmaceutical Society of Nigeria and the West African Postgraduate College of Pharmacists (WAPCP), she also holds a Masters’ in Public Health.',
+      'Dr Ama Ikuru is the Ag. Director, Capacity Building, NCDMB. A fellow of the Nigerian Society of Engineers. He also holds a Masters in Aeronautics.',
   },
   {
     id: 4,
-    name: 'Chidi Okoro',
-    title: 'Executive Consultant of Drug and Medicines.',
+    name: 'Engr. Simbi Wabote',
+    title: 'Executive Secretary, NCDMB',
     speakerImg: 'image/Simbi-wabote.jpg',
     about:
-      'Chidi Okoro teaches Strategy at Lagos Business School. He is currently the Founder/Executive Consultant of Drugs and Medicaments Nigeria Limited, a retail chain/own brands dealer in the Pharmaceutical sector.',
+      'Engr. Simbi Wabote teaches Strategy at Lagos Business School. He is currently the Founder/Executive IT Hub, Ikeja, Nigeria.',
   },
   {
     id: 5,
-    name: 'Abimbola Adebakin',
-    title: 'CEO, Advantage Health Africa',
+    name: 'Rose Ndong',
+    title: 'Executive Commissioner ICT, NCC',
     speakerImg: 'image/rose-ndong.jpg',
     about:
-      'Abimbola is the Chief Executive Officer of Advantage Health Africa, an aggregator in the retail pharmacy space.',
+      'Rose Ndong is Executive Commissioner - Exploration & Acreage Management, DPR.',
   },
   {
     id: 6,
-    name: 'Adedotun Sulaiman',
-    title: 'Veteran Sports Journalist',
+    name: 'Victor Udeh',
+    title: 'Managing Director, Arian Capital Limited',
     speakerImg: 'image/victor-ude.jpg',
     about:
-      'Mr. Adedotun Sulaiman, MFR is a veteran Management and Strategy consultant, with over 25 years’ experience. He has been the head of 15 different companies and is the Executive Chairman of Arian Capital Limited.',
+      'Victor Udeh, MFR is a veteran Management and Strategy consultant, with over 25 years’ experience. He has been the head of 15 different companies and is the Executive Chairman of Arian Capital Limited.',
   },
 ];
 
-const speakers = document.querySelector('#new-comers');
-for (let i = 0; i < speakersDetails.length; i += 1) {
-  speakers.innerHTML += `
-            <div class="speaker-details" id="showforth">
-              <div class="talk">
-                <div class="talking">
-                    <div class="speaker-img">
-                      <img src="${speakersDetails[i].speakerImg}" alt="Image of Speak">
-                    </div>
-                    <div class="keep">
-                    <h2>${speakersDetails[i].name}</h2>
-                    <p><em>${speakersDetails[i].title}</em></p>
-                    <div class="select"></div>
-                    <p>${speakersDetails[i].about}</p>
-                    </div>
-                </div>
-              </div>
-           </div>`;
-}
+const speakersContainer = document.querySelector('#new-comers');
+speakersDetails.forEach((speaker) => {
+  const speakerDetails = `
+      <div class="speaker-details" id="showforth">
+        <div class="talk">
+          <div class="talking">
+            <div class="speaker-img">
+              <img src="${speaker.speakerImg}" alt="Image of Speaker">
+            </div>
+            <div class="keep">
+              <h2>${speaker.name}</h2>
+              <p><em>${speaker.title}</em></p>
+              <div class="select"></div>
+              <p>${speaker.about}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  speakersContainer.innerHTML += speakerDetails;
+});
